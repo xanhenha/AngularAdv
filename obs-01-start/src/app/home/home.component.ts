4204086e5741ec470ae4a1483949262f6f6b3b5a
Xanhenha <xanhenha@gmail.com>
2020-10-30 08:57:44 -0300

    Observables - usand pipes e operators

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -17,7 +18,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customObs = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
-        if(count === 2)
+        if(count === 5)
           observer.complete();
         if(count > 3)
           observer.error(new Error('count is greater than 3!'));
@@ -25,8 +26,13 @@ export class HomeComponent implements OnInit, OnDestroy {
       } ,1000);
     });
 
-    this.firstObs = customObs.subscribe(count => {
-      console.log(count);
+    this.firstObs = customObs.pipe( filter( (data: number) => {
+      return data % 2 !== 0;
+    }), map( (data: number) => {
+      return 'Round: ' + (data + 1);
+    }))
+      .subscribe(data => {
+      console.log(data);
     }, error => {
       alert(error.message);
     }, () => {
