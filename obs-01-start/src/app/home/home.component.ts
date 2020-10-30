115faaadb087dd2ae8a91a88164605ac7ef8b345
Xanhenha <xanhenha@gmail.com>
2020-10-30 08:26:40 -0300

    Observables - Entendendo e criando um observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObs: Subscription
 
   constructor() { }
 
   ngOnInit() {
+    this.firstObs = interval(1000).subscribe(count => {
+      console.log(count)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObs.unsubscribe();
   }
 
 }
