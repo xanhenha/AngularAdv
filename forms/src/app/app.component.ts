1d11505eca43f8894cf09454efbdf7ae0f506fe1
Xanhenha <xanhenha@gmail.com>
2020-11-05 23:05:52 -0300

    Forms - Template-Drive - Final

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('form', {static: false}) form: NgForm;
+  defaultOption = 'pet';
+  answer = '';
+  submit = false;
+  genders = ['male', 'female'];
+  defaultGende = 'female';
+
+  user = {
+    name: '',
+    mail: '',
+    question: '',
+    answer: '',
+    gender: ''
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log('Submitted!', form);
+  // }
+
+  onSubmit() {
+    const data = this.form.value
+    this.submit = true;
+    this.user.name = data.userData.username;
+    this.user.mail = data.userData.email;
+    this.user.question = data.secret;
+    this.user.answer = data.questionAnswer;
+    this.user.gender = data.gender;
+    console.log(this.form)
+
+    // reset form
+    this.form.reset()
   }
 }
